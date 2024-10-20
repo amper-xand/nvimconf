8887b50a6b5f99356b08059bd381cd72a5a31d32
amper-xand <173635208+amper-xand@users.noreply.github.com>
2024-10-20 12:14:08 -0600

    plugin(telescope): added git status picker

--- lua/config/plugins/utils/telescope.lua
+++ lua/config/plugins/utils/telescope.lua
@@ -4,19 +4,22 @@ return {
     lazy = true,
     init = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope fuzzy find files" })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope fuzzy find buffers" })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope fuzzy find help tags" })
 
-        vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, {})
+        vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find,
+            { desc = "Telescope current buffer fuzzy find" })
 
-        vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
-        vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
-        vim.keymap.set('n', '<leader>fR', builtin.registers, {})
+        vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = "Telescope fuzzy find quickfix" })
+        vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = "Telescope fuzzy find jumplist" })
+        vim.keymap.set('n', '<leader>fR', builtin.registers, { desc = "Telescope fuzzy find registers" })
 
-        vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
-        vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+        vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = "Telescope fuzzy find lsp references" })
+        vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = "Telescope fuzzy find diagnostics" })
+
+        vim.keymap.set('n', '<leader>fGD', builtin.git_status, { desc = "Telescope fuzzy find with git status" })
     end,
     dependencies = { 'nvim-lua/plenary.nvim' }
 }
