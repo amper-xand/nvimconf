52c1d63693ae6826940ba0ab0477f3d0393d3b49
amper-xand <173635208+amper-xand@users.noreply.github.com>
2025-01-03 13:01:16 -0600

    misc(vimtex): change build config

--- lua/config/plugins/other/vimtex.lua
+++ lua/config/plugins/other/vimtex.lua
@@ -6,11 +6,11 @@ return {
         vim.g.vimtex_view_general_viewer = '/mnt/c/Users/andres/AppData/Local/SumatraPDF/SumatraPDF.exe'
         vim.g.vimtex_view_general_options = '@pdf'
 
-        vim.cmd [[
-        let g:vimtex_compiler_latexmk = {
-            \ 'out_dir' : './build',
-            \}
-            ]]
+        -- vim.cmd [[
+        -- let g:vimtex_compiler_latexmk = {
+        --     \ 'out_dir' : './build',
+        --     \}
+        --     ]]
 
         vim.g.vimtex_mappings_enabled = 1
 
